b4a1b40471d6c06cecdeeee0e4626be5d5a628e3
藪崎卓巳 <raru@syrup16g.net>
2018-12-18 12:46:11 +0900

    add sqls

---
+++ sql/dml/todo_table_seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO todo (id, name, content, created_at, updated_at) VALUES
+  (1, 'test1', 'test content1', now(), now()),
+  (2, 'test2', 'test content2', now(), now()),
+  (3, 'test3', 'test content3', now(), now()),
+  (4, 'test4', 'test content4', now(), now()),
+  (5, 'test5', 'test content5', now(), now());
