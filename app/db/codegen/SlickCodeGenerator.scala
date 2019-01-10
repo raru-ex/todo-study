11b711a1dee5c08dd766f71e75d0a6ebf1e36eca
raru-ex <raru@syrup16g.net>
2019-01-10 23:49:05 +0900

    change slik-codegenが動作するように調査と設定変更. warningは無視

--- app/db/codegen/SlickCodeGenerator.scala
+++ app/db/codegen/SlickCodeGenerator.scala
@@ -1,31 +1,17 @@
 package net.syrup16g.todo.db.codegen
 
 import slick.codegen.SourceCodeGenerator
-
 object SlickCodeGenerator extends App {
   //TODO: application.confから取得
-//  SourceCodeGenerator.run(
-//    profile = "slick.jdbc.MySQLProfile",
-//    jdbcDriver = "com.mysql.cj.jdbc.Driver",
-//    url = "jdbc:mysql://localhost:3306/todo",
-//    outputDir = "./output",
-//    pkg = "net.syrup16g.todo.model.db",
-//    user = Some("todo"),
-//    password = Some("password"),
-//    ignoreInvalidDefaults = true
-//  )
-
-
-  SourceCodeGenerator.main(
-    Array(
-      "slick.jdbc.MySQLProfile",
-      "com.mysql.cj.jdbc.Driver",
-      "jdbc:mysql://127.0.0.1:3306/todo",
-      "./output",
-      "net.syrup16g.todo.slick.model.db",
-      "todo",
-      "password"
-    )
+  SourceCodeGenerator.run(
+    profile = "slick.jdbc.MySQLProfile",
+    jdbcDriver = "com.mysql.cj.jdbc.Driver",
+    url = "jdbc:mysql://localhost:3306/todo?useSSL=false&nullNamePatternMatchesAll=true",
+    outputDir = "./output",
+    pkg = "net.syrup16g.todo.model.db",
+    user = Some("todo"),
+    password = Some("password"),
+    ignoreInvalidDefaults = true
   )
 
 }
