c6b5ce2defc172d34e7336ffb3e9223bada686fa
藪崎卓巳 <raru@syrup16g.net>
2019-02-26 00:10:33 +0900

    change slick3.2.3 -> 3.3

--- app/db/codegen/SlickCodeGenerator.scala
+++ app/db/codegen/SlickCodeGenerator.scala
@@ -1,17 +1,19 @@
 package net.syrup16g.todo.db.codegen
 
 import slick.codegen.SourceCodeGenerator
+
 object SlickCodeGenerator extends App {
   //TODO: application.confから取得
-  SourceCodeGenerator.run(
-    profile = "slick.jdbc.MySQLProfile",
-    jdbcDriver = "com.mysql.cj.jdbc.Driver",
-    url = "jdbc:mysql://localhost:3306/todo?useSSL=false&nullNamePatternMatchesAll=true",
-    outputDir = "./output",
-    pkg = "net.syrup16g.todo.model.db",
-    user = Some("todo"),
-    password = Some("password"),
-    ignoreInvalidDefaults = true
+
+  val profile = "slick.jdbc.MySQLProfile"
+  val driver  = "com.mysql.cj.jdbc.Driver"
+  val url     = "jdbc:mysql://localhost:3306/todo?useSSL=false&nullNamePatternMatchesAll=true"
+  val outputDir = "./output"
+  val pkg       = "net.syrup16g.todo.db.slick"
+  val user      = "todo"
+  val password  = "password"
+  SourceCodeGenerator.main(
+    Array(profile, driver, url, outputDir, pkg, user, password)
   )
 
 }
