e963ba5a2b42b31e03115fbce4e1e31a706640c2
藪崎卓巳 <raru@syrup16g.net>
2019-03-14 00:58:28 +0900

    change xml handlerをjson hanlderを参考に実装

--- app/http/XmlHttpErrorHandler.scala
+++ app/http/XmlHttpErrorHandler.scala
@@ -1,13 +1,84 @@
 package http
 
-import play.api.http.HttpErrorHandler
-import play.api.mvc.{RequestHeader, Result}
+import javax.inject.Inject
+import play.api.{Environment, Mode, OptionalSourceMapper}
+import play.api.http.{HttpErrorHandler, HttpErrorHandlerExceptions}
+import play.api.mvc.{RequestHeader, Result, Results}
+import play.api.mvc.Results.InternalServerError
+import play.core.SourceMapper
 
 import scala.concurrent.Future
+import scala.util.control.NonFatal
 
-class XmlHttpErrorHandler extends HttpErrorHandler {
+/**
+  * JsonHttpErrorHandlerを参考にxml用のエラーハンドラを作成
+  * @param environment
+  * @param sourceMapper
+  */
+class XmlHttpErrorHandler (environment: Environment, sourceMapper: Option[SourceMapper] = None)
+  extends HttpErrorHandler {
 
-  override def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = ???
+  /**
+    * 環境情報をInjectすることで開発環境、テスト環境で処理を分けることが可能
+    * @param environment
+    * @param optionalSourceMapper
+    * @return
+    */
+  @Inject
+  def this(environment: Environment, optionalSourceMapper: OptionalSourceMapper) = {
+    this(environment, optionalSourceMapper.sourceMapper)
+  }
 
-  override def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = ???
+  /**
+    * 400系エラー発生時に呼び出される処理
+    * @param request
+    * @param statusCode
+    * @param message
+    * @return
+    */
+  override def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
+    if (play.api.http.Status.isClientError(statusCode)) {
+      Future.successful(Results.Status(statusCode)(
+        <error>
+          <requestId>{request.id}</requestId>
+          <message>{message}</message>
+        </error>
+      ))
+    } else {
+      throw new IllegalArgumentException(
+        s"onClientError invoked with non client error status code $statusCode: $message"
+      )
+    }
+  }
+
+  /**
+    * 500系エラー発生時に呼び出される処理。
+    * @param request
+    * @param exception
+    * @return
+    */
+  override def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
+    try {
+      val isProd = environment.mode == Mode.Prod
+      val usefulException = HttpErrorHandlerExceptions.throwableToUsefulException(
+        sourceMapper,
+        isProd,
+        exception
+      )
+
+      Future.successful(InternalServerError(
+        <error>
+          <id>{usefulException.id}</id>
+          <requestId>{request.id}</requestId>
+          <exception>
+            <title>{usefulException.title}</title>
+            <description>{usefulException.description}</description>
+            <stacktrace>{usefulException.cause}</stacktrace>
+          </exception>
+        </error>
+      ))
+    } catch {
+      case NonFatal(e) => Future.successful(InternalServerError)
+    }
+  }
 }
