b919183531bfe5a3b72f694a70375242eef27454
raru-ex <raru@syrup16g.net>
2019-03-18 23:42:53 +0900

    change conten-typeによるエラーハンドリングサンプルの追加

--- app/http/CustomContentTypeErrorHandler.scala
+++ app/http/CustomContentTypeErrorHandler.scala
@@ -1,4 +1,4 @@
-package http
+package net.syrup16g.http
 
 import javax.inject.Inject
 import play.api.http._
--- app/http/XmlHttpErrorHandler.scala
+++ app/http/XmlHttpErrorHandler.scala
@@ -1,14 +1,36 @@
-package http
+/**
+  * Note:
+  *    Reference of the source code from the following point of view.
+  *  ・Depending on the function of ConfigLoader.
+  *    Behavior of easily obtaining the value of specified type via type function of get function
+  *
+  *  https://github.com/playframework/playframework/blob/master/core/play/src/main/scala/play/api/http/HttpErrorHandler.scala
+  *
+  * Copyright (C) 2009-2019 Lightbend Inc. (https://www.lightbend.com).
+  *   Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this project except in compliance with the License.
+  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+  *
+  * Unless required by applicable law or agreed to in writing,
+  * software distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+  * either express or implied. See the License for
+  * the specific language governing permissions and limitations under the License.
+*/
+
+package net.syrup16g.http
 
 import javax.inject.Inject
-import play.api.{Environment, Mode, OptionalSourceMapper}
+import play.api._
 import play.api.http.{HttpErrorHandler, HttpErrorHandlerExceptions}
 import play.api.mvc.{RequestHeader, Result, Results}
 import play.api.mvc.Results.InternalServerError
 import play.core.SourceMapper
+import play.libs.exception.ExceptionUtils
 
 import scala.concurrent.Future
 import scala.util.control.NonFatal
+import scala.xml.Elem
 
 /**
   * JsonHttpErrorHandlerを参考にxml用のエラーハンドラを作成
@@ -17,6 +39,7 @@ import scala.util.control.NonFatal
   */
 class XmlHttpErrorHandler (environment: Environment, sourceMapper: Option[SourceMapper] = None)
   extends HttpErrorHandler {
+  private val logger: Logger = Logger.apply(classOf[XmlHttpErrorHandler])
 
   /**
     * 環境情報をInjectすることで開発環境、テスト環境で処理を分けることが可能
@@ -65,7 +88,7 @@ class XmlHttpErrorHandler (environment: Environment, sourceMapper: Option[Source
         isProd,
         exception
       )
-
+      logServerError(request, usefulException)
       Future.successful(InternalServerError(
         <error>
           <id>{usefulException.id}</id>
@@ -73,12 +96,34 @@ class XmlHttpErrorHandler (environment: Environment, sourceMapper: Option[Source
           <exception>
             <title>{usefulException.title}</title>
             <description>{usefulException.description}</description>
-            <stacktrace>{usefulException.cause}</stacktrace>
+            <stacktrace>{formatServerErrorException(usefulException.cause)}</stacktrace>
           </exception>
         </error>
       ))
     } catch {
       case NonFatal(e) => Future.successful(InternalServerError)
     }
+  }
+
+  /**
+    * エラーログ出力
+    * playのソースを流用
+    */
+  private def logServerError(request: RequestHeader, usefulException: UsefulException): Unit = {
+    // Logger.errorの呼び方はdeprecatedになっているため、呼び方を修正
+    logger.error(
+      """
+        |
+        |! @%s - Internal server error, for (%s) [%s] ->
+        | """.stripMargin.format(usefulException.id, request.method, request.uri),
+      usefulException
+    )
+  }
+
+  /**
+    * stacktraceの各行をxmlのlineタグにして返す
+    */
+  private def formatServerErrorException(exception: Throwable): Array[Elem] = {
+    ExceptionUtils.getStackFrames(exception).map(s => <line>{s.trim}</line>)
   }
 }
