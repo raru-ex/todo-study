648e17e2bab7ce59684360382ddb0ef01f714d06
raru-ex <raru@syrup16g.net>
2019-06-12 00:26:31 +0900

    change formの処理を修正

--- app/controllers/user/RegisterController.scala
+++ app/controllers/user/RegisterController.scala
@@ -3,37 +3,38 @@ package net.syrup16g.todo.controllers.user
 import play.api.mvc._
 import javax.inject.{Singleton, Inject}
 import play.api.i18n.I18nSupport
-import net.syrup16g.todo.viewmodels.UserViewModel.signUpForm
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import net.syrup16g.todo.repositories.UserRepository
 import net.syrup16g.todo.db.model.User
 import scala.concurrent.ExecutionContext.Implicits._
+import scala.concurrent.Future
+import net.syrup16g.todo.form._
 
 @Singleton
 class RegisterController @Inject()(
   cc: MessagesControllerComponents
 ) extends AbstractController(cc) with I18nSupport {
+  val signUpForm = UserForm.createSignUpForm
 
   /**
    * 画面表示
    */
   def create() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.user.create())
+    Ok(views.html.user.create(signUpForm))
   }
 
-  def store() = Action { implicit request: Request[AnyContent] =>
+  def store() = Action async { implicit request: Request[AnyContent] =>
     signUpForm.bindFromRequest.fold(
       errors => {
-        BadRequest(views.html.user.create())
+        Future.successful(BadRequest(views.html.user.create(errors)))
       },
       form => {
         val bcryptEncoder = new BCryptPasswordEncoder()
-        val password = bcryptEncoder.encode(form._3._1)
-        val newUser = User(None, form._1, form._2, password)
+        val password = bcryptEncoder.encode(form.password.main)
+        val newUser = User(None, form.nickname, form.mail, password)
         for {
           idOpt <- UserRepository.insert(newUser)
-        } yield NoContent
+        } yield Redirect(routes.RegisterController.create())
       })
-    Ok(views.html.user.create())
   }
 }
---
+++ app/form/User.scala
@@ -0,0 +1,25 @@
+package net.syrup16g.todo.form
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.validation.Constraints
+
+// ========== Form Mapped case class
+case class SignUp(nickname: String, mail: String, password: PasswordConfirm)
+case class PasswordConfirm(main: String, confirm: String)
+
+/**
+ * User related From creator
+ */
+object UserForm {
+
+  def createSignUpForm: Form[SignUp] = Form(
+    mapping(
+      "nickname" -> nonEmptyText(maxLength = 255),
+      "mail"     -> nonEmptyText.verifying(Constraints.emailAddress),
+      "password" -> mapping(
+          "main"    -> nonEmptyText,
+          "confirm" -> nonEmptyText
+        )(PasswordConfirm.apply)(PasswordConfirm.unapply).verifying(password => password.main == password.confirm)
+      )(SignUp.apply)(SignUp.unapply)
+    )
+}
